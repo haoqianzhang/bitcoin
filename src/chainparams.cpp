fea4fc13a5d76bc857bf9eb6818077d530fc9373
zhq422194011 <haoqianzhang@pku.edu.cn>
2018-05-19 22:22:01 +0800

    changing chainTxData

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -170,10 +170,10 @@ public:
 
         chainTxData = ChainTxData{
             // Data as of block 0000000000000000002d6cca6761c99b3c2e936f9a0e304b7c7651a993f461de (height 506081).
-            1516903077, // * UNIX timestamp of last known number of transactions
-            295363220,  // * total number of transactions between genesis and that timestamp
+            0, // * UNIX timestamp of last known number of transactions
+            0,  // * total number of transactions between genesis and that timestamp
                         //   (the tx=... number in the SetBestChain debug.log lines)
-            3.5         // * estimated number of transactions per second after that timestamp
+            0         // * estimated number of transactions per second after that timestamp
         };
     }
 };
